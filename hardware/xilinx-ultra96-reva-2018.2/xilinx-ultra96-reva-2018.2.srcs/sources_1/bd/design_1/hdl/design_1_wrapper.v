ddb00e1d298bf8e3369048e82684ebff6aaa83a1
Jan Schiefer <jschiefer@users.noreply.github.com>
2019-04-21 21:36:07 -0700

    Add clocks, resets, interrupts and AXI ports for SDSoC.

--- hardware/xilinx-ultra96-reva-2018.2/xilinx-ultra96-reva-2018.2.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ hardware/xilinx-ultra96-reva-2018.2/xilinx-ultra96-reva-2018.2.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-//Date        : Fri Apr 19 00:30:42 2019
+//Date        : Sun Apr 21 00:26:13 2019
 //Host        : whitebox running 64-bit Linux Mint 18.3 Sylvia
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
